807f785f127391362e134d89efc2f0447b448cf3
ArnoSchiller <arno-schiller@hotmail.de>
2026-05-05 12:44:54 +0200

    Added missing value QUANTITY_PRICE for PriceDesignationEnum

--- Source/FikaAmazonAPI/AmazonSpApiSDK/Models/OrdersV20260101/PriceDesignationEnum.cs
+++ Source/FikaAmazonAPI/AmazonSpApiSDK/Models/OrdersV20260101/PriceDesignationEnum.cs
@@ -9,6 +9,10 @@ namespace FikaAmazonAPI.AmazonSpApiSDK.Models.OrdersV20260101
 {
     /// <summary>
     /// Indicates that the selling price is a special price that is only available for Amazon Business orders. For more information about the Amazon Business Seller Program, refer to the Amazon Business website.
+    /// 
+    /// Note: Official Amazon API documentation (as of 2025) states only BUSINESS_PRICE is possible,
+    /// but the API actually returns additional values like QUANTITY_PRICE for tiered pricing.
+    /// See: https://developer-docs.amazon.com/sp-api/docs/orders-api-v0-reference
     /// </summary>
     /// <value>Indicates that the selling price is a special price that is only available for Amazon Business orders. For more information about the Amazon Business Seller Program, refer to the Amazon Business website.</value>
     [JsonConverter(typeof(StringEnumConverter))]
@@ -19,6 +23,13 @@ namespace FikaAmazonAPI.AmazonSpApiSDK.Models.OrdersV20260101
         /// </summary>
         [EnumMember(Value = "BUSINESS_PRICE")]
         BUSINESS_PRICE = 1,
+
+        /// <summary>
+        /// Enum value for type QUANTITY_PRICE (tiered/quantity-based pricing)
+        /// Note: This value is returned by the API but not documented in the official API reference as of 2025.
+        /// </summary>
+        [EnumMember(Value = "QUANTITY_PRICE")]
+        QUANTITY_PRICE = 2
     }
 
 }
